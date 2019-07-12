37d157f6c36a24c14f643bc52afe8704967f5ee0
= <=>
2019-07-12 13:52:32 +0900

    Initial Commit

---
+++ IH03041/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IH03041
+//
+//  Created by 藤原吉司 on 2019/07/12.
+//  Copyright © 2019 local.hal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IH03041Tests/IH03041Tests.swift
@@ -0,0 +1,34 @@
+//
+//  IH03041Tests.swift
+//  IH03041Tests
+//
+//  Created by 藤原吉司 on 2019/07/12.
+//  Copyright © 2019 local.hal. All rights reserved.
+//
+
+import XCTest
+@testable import IH03041
+
+class IH03041Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
